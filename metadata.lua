1de955f03d1ee9f246dd47ae6c4445e48c05405f
Han Li <aooohan@gmail.com>
2025-09-29 10:54:33 +0000

    v0.1.7

--- metadata.lua
+++ metadata.lua
@@ -5,7 +5,7 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "cmake"
 --- Plugin version
-PLUGIN.version = "0.1.6"
+PLUGIN.version = "0.1.7"
 --- Plugin homepage
 PLUGIN.homepage = "https://github.com/version-fox/vfox-cmake"
 --- Plugin license, please choose a correct license according to your needs.
